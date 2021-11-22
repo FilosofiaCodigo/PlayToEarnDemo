7902c79c0bc763e739d68d0df2091776c0c62655
Ahmed Castro <ahmed.hn.43@gmail.com>
2021-11-22 09:28:45 -0600

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
   constructor () ERC20("Token", "TKN") {
     _mint(msg.sender, 1000 ether);
-  }
-
-  function mint(address account, uint256 amount) public {
-    _mint(account, amount);
   }
 }
