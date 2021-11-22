acc8288cf9be7220c73458150c5abbcfb0015797
Ahmed Castro <ahmed.hn.43@gmail.com>
2021-11-22 09:28:56 -0600

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
   constructor () ERC20("Token", "TKN") {
-    _mint(msg.sender, 1000 ether);
+    _mint(msg.sender, 1000000000 ether);
   }
 }
